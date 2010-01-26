baa080cdba186166b740c2585e45c6334d066747
Amiel Martin <amiel@carnesmedia.com>
2010-01-26 11:11:37 -0800

    initial import

---
+++ tabularosa.js
@@ -0,0 +1,24 @@
+/*
+ * tabularosa.js
+ * Amiel Martin
+ * 2010-01-26
+ *
+ * Placeholder text as an extremely simple jquery plugin.
+ */
+
+$.fn.tabularosa = function(value) {
+	return this.each(function() {
+		var self = $(this),
+			klass = 'tabularosa',
+			value = value || self.attr('data-tabularosa'),
+			set_value = function() {
+				if ($.trim(self.val()) == '' || self.val() == value)
+					self.val(value).addClass(klass);
+			},
+			clear_value = function() {
+				if (self.val() == value)
+					self.val('').removeClass(klass);
+			};
+		self.focus(clear_value).blur(set_value).blur();
+	});
+};
