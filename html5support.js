68b9af9774f64d7a2395a59baefe0c1db106704c
Amiel Martin <amiel.martin@gmail.com>
2010-02-04 22:22:06 -0800

    mini refactor

--- html5support.js
+++ html5support.js
@@ -1,24 +1,24 @@
 /*
- * tabularosa.js
+ * html5support.js
  * Amiel Martin
  * 2010-01-26
  *
- * Add placeholder for browsers that don't support it.
+ * Support certain HTML 5 attributes with javascript, but only if the browser doesn't already support them.
  */
 
-var HTML5Support = {};
-
-(function($){
+var HTML5Support = (function($){
 	var // private members
-	// if no value is specified, find placeholder text in this html attribute
-	placeholder_attribute = 'placeholder',
-	// give the input field this class when the placeholder text is used
-	placeholder_klass = placeholder_attribute;
+		// if no value is specified, find placeholder text in this html attribute
+		placeholder_attribute = 'placeholder',
+		// give the input field this class when the placeholder text is used
+		placeholder_klass = placeholder_attribute,
+		// this will become our HTML5Support object
+		h5 = {};
 	
 	// public functions
-	$.extend(HTML5Support, {
+	$.extend(h5, {
 		supports_attribute: function(attribute, type) { // should we memoize this?
-			return !!(attribute in document.createElement(type || 'input'));
+			return attribute in document.createElement(type || 'input');
 		}
 	});
 
@@ -41,12 +41,12 @@ var HTML5Support = {};
 	// jquery plugins
 	
 	$.fn.placeholder = function(value) {
-		if (HTML5Support.supports_attribute('placeholder')) return this;
+		if (h5.supports_attribute('placeholder')) return this;
 		return this.each(tabularosa);
 	};
 	
 	$.fn.autofocus = function() {
-		if (HTML5Support.supports_attribute('autofocus')) return this;
+		if (h5.supports_attribute('autofocus')) return this;
 		return this.focus();
 	};
 	
@@ -55,4 +55,6 @@ var HTML5Support = {};
 	$.placeholder = function() { $('['+placeholder_attribute+']').placeholder(); };
 	
 	$.html5support = function() { $.autofocus(); $.placeholder(); };
+	
+	return h5;
 })(jQuery);
