cbaff25df6e0413867b8bf2184b85bc831b0d8a3
Amiel Martin <amiel@carnesmedia.com>
2010-04-19 18:22:00 -0700

    yay, more tests

--- test/test.js
+++ test/test.js
@@ -42,18 +42,60 @@ $(document).ready(function() {
 	module("Password placeholder");
 	if (HTML5Support.supports_attribute('placeholder')) {
 	    test("with placeholder support", function() {
-	        var input = $('#password_field');
-	        equal(3, $('#test_form input').length, "sanity check, we should have 3 input fields");
+	        var input = $('#password_field'),
+	            n_fields = $('#test_form input').length;
 	        input.placeholder();
-	        equal(3, $('#test_form input').length, "there should still be 3 input fields");
+	        equal($('#test_form input').length, n_fields, "expected the number of fields not to change");
 	    });
 	} else {
 	    test("without placeholder support", function() {
-	        var input = $('#password_field');
-	        equal(3, $('#test_form input').length, "sanity check, we should have 3 input fields");
+	        var input = $('#password_field'),
+	            n_fields = $('#test_form input').length;
+	        
 	        input.placeholder();
-	        equal(4, $('#test_form input').length, "there should still be 3 input fields");
+	        
+	        var placeholder = input.next();
+	        equal($('#test_form input').length, n_fields+1, "there should still be 4 input fields");
+	        equal(placeholder.length, 1, 'we expect the new placeholder input to exist');
 	        ok(!input.is(':visible'), 'the input field should have been hidden');
+	        ok(placeholder.is(':visible'), "we should start out by seeing the placeholder");
+	        
+	        placeholder.focus();
+	        
+	        ok(input.is(':visible'), 'the input field should have been shown');
+	        ok(!placeholder.is(':visible'), 'the placeholder should have been hidden');
+	        
+	        // we could go through a whole cycle, but the next test does...
+	        
+	        input.blur();
+	    });
+	    
+	    test("without placeholder support and with already a value in the password field", function() {
+	        var input = $('#password_with_value'),
+	            n_fields = $('#test_form input').length;
+
+	        input.placeholder();
+	        
+	        var placeholder = input.next();
+	        equal($('#test_form input').length, n_fields+1, "there should still be 3 input fields");
+	        equal(placeholder.length, 1, 'we expect the new placeholder input to exist');
+	        ok(input.is(':visible'), 'the input field should have been hidden');
+	        ok(!placeholder.is(':visible'), "we should start out by seeing the placeholder");
+	        
+	        input.focus();
+	        
+	        ok(input.is(':visible'), 'the input field should still be shown');
+	        ok(!placeholder.is(':visible'), 'the placeholder should still be hidden');
+	        
+	        input.val(' '); input.blur();
+	        
+	        ok(!input.is(':visible'), 'the input field should have been hidden');
+	        ok(placeholder.is(':visible'), "the placeholder should have been shown");
+	        
+	        placeholder.focus(); input.val('secret').blur();
+	        
+	        ok(input.is(':visible'), 'the input field should be shown');
+	        ok(!placeholder.is(':visible'), 'the placeholder should be hidden');
 	    });
 	}
 	
